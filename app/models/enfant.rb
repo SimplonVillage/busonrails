ae5d3149671444279414b004acb169b15c05fbde
remy <remy.peyre05@gmail.com>
2015-01-22 12:07:51 +0100

    correction formulaire enfant champs prenom

--- app/models/enfant.rb
+++ app/models/enfant.rb
@@ -3,7 +3,7 @@ class Enfant < ActiveRecord::Base
 
   validates_presence_of :datenaissance, :ecole, :classe
   validates :nomenfant, format: { with: /\A[A-Za-zé\.¸UûÙùàÀèÈéÉïÏîÎôÔêÊçÇ \,\\\'-]+\z/, message: I18n.t('errors.messages.not_a_string') } 
-  validates :prenom, format: { with: /\A[A-Za-zé\.¸UûÙùàÀèÈéÉïÏîÎôÔêÊçÇ \,\\\'-]+\z/, message: I18n.t('errors.messages.not_a_string') } 
+  validates :prenomenfant, format: { with: /\A[A-Za-zé\.¸UûÙùàÀèÈéÉïÏîÎôÔêÊçÇ \,\\\'-]+\z/, message: I18n.t('errors.messages.not_a_string') } 
 
 
 end
