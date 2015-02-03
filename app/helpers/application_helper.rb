f15031be73bc0ce81c5d93747d621bdf3e73747b
remy <remy.peyre05@gmail.com>
2015-02-03 16:45:25 +0100

    Affichage message d'erreur dans formulaire
    	*modif pour mettre erreur a coté du champ
    	*modif du formulaire

--- app/helpers/application_helper.rb
+++ app/helpers/application_helper.rb
@@ -3,7 +3,7 @@ module ApplicationHelper
 	def error_message_for(resource,field, options = {:prepend_text => ""})
 	  error_message = resource.errors[field][0]
 	  if error_message
-	    raw "<span class='errors'>#{options[:prepend_text]} #{error_message} </span>"
+	    raw "<div class='alert alert-warning' role='alert'><span class='errors'>#{options[:prepend_text]} #{error_message} </span></div>"
 	  end
   end
 
