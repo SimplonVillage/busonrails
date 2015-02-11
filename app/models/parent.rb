55d0b91fee98516e443a4be806f388931727cf43
remy <remy.peyre05@gmail.com>
2015-02-11 13:34:41 +0100

    Modification du formulaire Parent
    	*Mis en place de placeholder dans les champs
    	*Suppresion des tel prof

--- app/models/parent.rb
+++ app/models/parent.rb
@@ -16,7 +16,7 @@ class Parent < ActiveRecord::Base
                         :civilite2
 
 
-  #validates :teldom, presence: true, numericality: true, length: { is: 10 }
+  validates :teldom, presence: true, numericality: true, length: { is: 10 }
   validates :telport1, presence: true, numericality: true, length: { is: 10 }
   validates :telport2, presence: true, numericality: true, length: { is: 10 }
   # validates :telpro1, numericality: true, length: { is: 10 }
